3aab5fa235c33abf8f305ddc424b34d8cb7406d6
Supakorn Trakulmaiphol <supakorn.t@ibm.com>
2023-09-08 11:09:26 +0700

    ADD

--- thailand-service/index.js
+++ thailand-service/index.js
@@ -1,11 +1,20 @@
-const express = require('express')
-const app = express()
-const port = 3000
+var express = require('express')
+var bodyParser = require('body-parser')
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
+var app = express()
+
+// create application/json parser
+var jsonParser = bodyParser.json()
+
+// create application/x-www-form-urlencoded parser
+var urlencodedParser = bodyParser.urlencoded({ extended: false })
+
+// POST /login gets urlencoded bodies
+app.post('/login', urlencodedParser, function (req, res) {
+  res.send('welcome, ' + req.body.username)
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+// POST /api/users gets JSON bodies
+app.post('/api/users', jsonParser, function (req, res) {
+  // create user in req.body
 })
